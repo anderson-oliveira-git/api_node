bfa9e0abeccea333f6d71db307343a8ce046fbc5
unknown <andersonreinilson@hotmail.com>
2023-07-08 17:20:08 -0300

    style: alteração no código api/database/dbConfig.js

--- api/database/config/dbConfig.js
+++ api/database/config/dbConfig.js
@@ -4,15 +4,15 @@ require('dotenv').config({
 
 module.exports = {
   development: {
-    username : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE,
-    host     : process.env.DB_HOST,
-    port     : process.env.DB_PORT,
-    dialect  : process.env.DB_DIALECT,
+    username : process.env.DB_USERNAME || 'root',
+    password : process.env.DB_PASSWORD || '',
+    database : process.env.DB_DATABASE || 'development_db',
+    host     : process.env.DB_HOST || '127.0.0.1',
+    port     : process.env.DB_PORT || 3306,
+    dialect  : process.env.DB_DIALECT || 'mysql',
     define   : {
-        timestamps : process.env.DB_TIMESTAMPS
+        timestamps : process.env.DB_TIMESTAMPS || true
     },
-    timezone : process.env.DB_TIMEZONE
+    timezone : process.env.DB_TIMEZONE || '-03:00'
   }
 }
